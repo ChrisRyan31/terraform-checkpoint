5f21c6089f16ca33db415b0320d80100ed8e2630
Holman-StateFarm <chris.holman.mkjw@statefarm.com>
2021-11-15 14:37:58 -0600

    fixed egress rule

--- main.tf
+++ main.tf
@@ -82,13 +82,12 @@ resource "aws_security_group" "security-group-terraform-checkpoint" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "terraformed-security-group-checkpoint"
